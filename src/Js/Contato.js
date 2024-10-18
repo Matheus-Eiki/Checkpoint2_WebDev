f77d8c7c9df6d2ac7202196fb09b91e9680eca3b
Matheus <m.eiki.ikeda@gmail.com>
2024-10-18 16:51:59 -0300

    Criando uma funcao para enviar e receber os dados de contato

--- src/Js/Contato.js
+++ src/Js/Contato.js
@@ -1,3 +1,19 @@
 function trocar(cor) {
     document.body.style.background = cor;
 }
+/*O código armazena os valores dos input
+Após clicar no botão enviar ele traz um alerta na tela
+Limpa os campos de input atribuindo uma str vazia
+*/
+
+function botaoenviar() {
+    var input = document.getElementById('nome').value;
+    var input = document.getElementById('telefone').value;
+    var input = document.getElementById('email').value;
+
+    alert("Dados enviados com sucesso");
+
+    document.getElementById('nome').value = '';
+    document.getElementById('telefone').value = '';
+    document.getElementById('email').value = '';
+}  
