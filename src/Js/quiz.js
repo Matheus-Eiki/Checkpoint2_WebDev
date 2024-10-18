4903ea5388520378202ffe480ba84c69f3d1bde2
Matheus <m.eiki.ikeda@gmail.com>
2024-10-18 16:48:48 -0300

    Criando uma funcao para calcular o resultado do quiz

--- src/Js/quiz.js
+++ src/Js/quiz.js
@@ -2,3 +2,20 @@ alert("Vamos conhecer sobres bikes")
 function trocar(cor) {
     document.body.style.background = cor;
 }
+function calcularResultado() {
+
+    let scoreA = 0;
+    let scoreB = 0;
+    let scoreC = 0;
+  
+    const respostas = document.querySelectorAll('input[type="radio"]:checked');
+    
+    respostas.forEach((resposta) => {
+      if (resposta.value === "A") {
+        scoreA++;
+      } else if (resposta.value === "B") {
+        scoreB++;
+      } else if (resposta.value === "C") {
+        scoreC++;
+      }
+    });
