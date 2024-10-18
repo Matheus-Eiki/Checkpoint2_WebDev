f8b3c6efcd9ec265ef3ba5383616073a88749383
Matheus <m.eiki.ikeda@gmail.com>
2024-10-18 16:50:00 -0300

    Criando uma funcao para enviar o resultado do quiz

--- src/Js/quiz.js
+++ src/Js/quiz.js
@@ -19,3 +19,17 @@ function calcularResultado() {
         scoreC++;
       }
     });
+    let resultado;
+    if (scoreA > scoreB && scoreA > scoreC) {
+      resultado = "Bicicleta de Corrida (Speed) – Ideal para você que busca alta performance em estradas pavimentadas e ciclismo de velocidade. A bike é leve, rápida e eficiente para treinos e competições.";
+    } else if (scoreB > scoreA && scoreB > scoreC) {
+      resultado = "Bicicleta de Trilha (Mountain Bike - MTB para trilhas moderadas) – Perfeita para quem adora misturar aventura e performance em trilhas de terra, com equilíbrio entre conforto e agilidade. Ótima para quem quer explorar trilhas mais leves e com obstáculos moderados.";
+    } else if (scoreC > scoreA && scoreC > scoreB) {
+      resultado = "Bicicleta de Montanha (Mountain Bike - MTB de alta performance) – Ideal para terrenos acidentados, subidas íngremes e aventuras off-road intensas. Perfeita para quem gosta de desafios e precisa de uma bike robusta, com maior resistência e controle sobre terrenos difíceis.";
+    } else {
+      resultado = "Bicicleta hibrida - ideal para todos os tipos de terrenos tanto para estradas pavimentados quanto para acidentadas";
+    }
+  
+
+    document.getElementById('resultado').innerText = resultado;
+  }
